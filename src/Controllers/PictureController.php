7007a57412903f2dcd9a0f910a6db8fbe12274bf
Leanna Pelham <leanna.pelham@knplabs.com>
2019-09-23 14:47:39 -0400

    adding date header

--- src/Controllers/PictureController.php
+++ src/Controllers/PictureController.php
@@ -22,7 +22,7 @@ class PictureController
                 <img src='show?file={$picture->getFileName()}' class='big-foot-img'/>
                 <p class='mt-3 mb-0'>Image taken by: <strong>{$picture->getAuthor()}</strong></p> 
                 <p class='mb-0'>Coordinates: <strong>{$picture->getLocation()}</strong></p>
-                <p class='mb-0'><strong>{$picture->getDate()->format('d/m/Y')}</strong></p>
+                <p class='mb-0'>Date: <strong>{$picture->getDate()->format('d/m/Y')}</strong></p>
             </div>
         ";
         }
