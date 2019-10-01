3ea66c7ee622a45fadf0cf5b6217c85a49cca81a
Ryan Weaver <ryan@thatsquality.com>
2019-10-01 14:48:39 -0400

    tweaking to match the Symfony coding standards

--- src/Controllers/PictureController.php
+++ src/Controllers/PictureController.php
@@ -14,31 +14,30 @@ class PictureController
         $this->renderer = new Renderer();
     }
 
-    public function index() : string
+    public function index(): string
     {
         $contents = '
         <p>All reported Sasquatch sightings, we will find the truth, upload your image to help us find Bigfoot!</p>
         <a class="btn btn-info btn-lg" href="upload">Upload your Bigfoot Image</a>
         <div class="my-5 row">';
 
-        foreach ( Picture::findAll() as $picture ) {
+        foreach (Picture::findAll() as $picture) {
             $contents .= $picture->render();
         }
 
         $contents .= '</div>';
-        
-        return $this->renderer->renderPage( 'All Reported Sightings', $contents );
+
+        return $this->renderer->renderPage('All Reported Sightings', $contents);
     }
 
-    public function upload() : string
+    public function upload(): string
     {
-        if ( strtolower( $_SERVER['REQUEST_METHOD'] ) == 'post' ) {
+        if ('post' == strtolower($_SERVER['REQUEST_METHOD'])) {
             // Process form
-            if ( !array_key_exists('newPicture', $_FILES) || !array_key_exists('author', $_POST) ) {
+            if (!array_key_exists('newPicture', $_FILES) || !array_key_exists('author', $_POST)) {
+                http_send_status(400);
 
-                http_send_status( 400 );
-
-                return $this->renderer->renderPage( 'New sighting',"<p>Sorry... I didn't understand :(...</p><p>Wanna <a href='upload'>try again?</a></p>");
+                return $this->renderer->renderPage('New sighting', "<p>Sorry... I didn't understand :(...</p><p>Wanna <a href='upload'>try again?</a></p>");
             } else {
                 try {
                     $newPicture = Picture::createFromUpload(
@@ -53,8 +52,8 @@ class PictureController
                 <h2>Sasquatch Spotted! </h2>
                 <p>Click <u><a class="text-white" href="/">here</a></u> to see your image and the rest of the sightings!</p>'
                     );
-                } catch ( \Exception $e ) {
-                    error_log( __FILE__.': '.$e->getMessage() );
+                } catch (\Exception $e) {
+                    error_log(__FILE__.': '.$e->getMessage());
 
                     return $this->renderer->renderPage('New sighting!', '
             <h2 class="mb-4 text-center">Sneaky like a Sasquatch, that upload didn\'t work!</h2>
@@ -64,13 +63,12 @@ class PictureController
                 }
             }
         } else {
-
             return $this->renderer->renderUploadForm();
         }
     }
 
-    public function show( string $fileName ) : string
+    public function show(string $fileName): string
     {
-        return file_get_contents( __DIR__.'/../../uploads/'.$fileName );
+        return file_get_contents(__DIR__.'/../../uploads/'.$fileName);
     }
 }
--- src/Models/Picture.php
+++ src/Models/Picture.php
@@ -9,7 +9,7 @@ class Picture
     private $fileName;
     private $location;
 
-    public function __construct( string $author, \DateTimeImmutable $date, string $location, string $fileName )
+    public function __construct(string $author, \DateTimeImmutable $date, string $location, string $fileName)
     {
         $this->author = $author;
         $this->date = $date;
@@ -17,7 +17,7 @@ class Picture
         $this->location = $location;
     }
 
-    public function getAuthor() : string
+    public function getAuthor(): string
     {
         return $this->author;
     }
@@ -38,18 +38,18 @@ class Picture
         return $this->location;
     }
 
-    public static function findAll() : array
+    public static function findAll(): array
     {
         $dir = new \DirectoryIterator(__DIR__.'/../../picture_info');
 
         $pictures = [];
-        foreach ( $dir as $fileInfo ) {
-            if ( $fileInfo->getExtension() == 'json' ) {
-                $metadata = json_decode( file_get_contents( $fileInfo->getPathname() ), true );
+        foreach ($dir as $fileInfo) {
+            if ('json' == $fileInfo->getExtension()) {
+                $metadata = json_decode(file_get_contents($fileInfo->getPathname()), true);
 
                 $pictures[] = new Picture(
                     $metadata['author'],
-                    new \DateTimeImmutable( $metadata['date'] ),
+                    new \DateTimeImmutable($metadata['date']),
                     $metadata['location'],
                     $metadata['fileName']
                 );
@@ -67,7 +67,7 @@ class Picture
         return $this->fileName;
     }
 
-    public function render() : string
+    public function render(): string
     {
         return "
             <div class='col-sm-4 mb-5'>
@@ -80,18 +80,19 @@ class Picture
     }
 
     /**
-     * @param array $uploadedFile
+     * @param array              $uploadedFile
      * @param \DateTimeImmutable $date
-     * @param string $author
-     * @param string $location
+     * @param string             $author
+     * @param string             $location
+     *
      * @return Picture
+     *
      * @throws \Exception
      */
-    public static function createFromUpload( array $uploadedFile, \DateTimeImmutable $date, string $author, string $location ) : Picture
+    public static function createFromUpload(array $uploadedFile, \DateTimeImmutable $date, string $author, string $location): Picture
     {
-        $destination = __DIR__ . '/../../uploads/' . basename($uploadedFile['name']);
-        if ( move_uploaded_file( $uploadedFile['tmp_name'], $destination ) ) {
-
+        $destination = __DIR__.'/../../uploads/'.basename($uploadedFile['name']);
+        if (move_uploaded_file($uploadedFile['tmp_name'], $destination)) {
             return new Picture(
                 $author,
                 $date,
@@ -99,22 +100,20 @@ class Picture
                 basename($uploadedFile['name'])
             );
         } else {
-
-            throw new \Exception( 'Couldn\'t store the upload :(' );
+            throw new \Exception('Couldn\'t store the upload :(');
         }
     }
 
     public function save()
     {
-        $destination = __DIR__ . '/../../picture_info/' . basename($this->getFileName());
-        $infoFileName = substr( $destination, 0, strrpos($destination, '.') ).'.json';
+        $destination = __DIR__.'/../../picture_info/'.basename($this->getFileName());
+        $infoFileName = substr($destination, 0, strrpos($destination, '.')).'.json';
 
-        file_put_contents( $infoFileName, json_encode( [
+        file_put_contents($infoFileName, json_encode([
             'date' => $this->getDate()->format('Y/m/d'),
             'author' => $this->getAuthor(),
             'location' => $this->getLocation(),
             'fileName' => basename($this->getFileName()),
-        ] ) );
-
+        ]));
     }
 }
