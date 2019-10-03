0e679842b9026bd3d8b708ec3b5a721910406a7c
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-03 11:14:03 -0300

    Added doc blocks

--- src/Models/Picture.php
+++ src/Models/Picture.php
@@ -9,6 +9,13 @@ class Picture
     private $fileName;
     private $location;
 
+    /**
+     * Picture constructor.
+     * @param string $author
+     * @param \DateTimeImmutable $date
+     * @param string $location
+     * @param string $fileName
+     */
     public function __construct(string $author, \DateTimeImmutable $date, string $location, string $fileName)
     {
         $this->author = $author;
@@ -17,21 +24,33 @@ class Picture
         $this->location = $location;
     }
 
+    /**
+     * @return string
+     */
     public function getAuthor(): string
     {
         return $this->author;
     }
 
+    /**
+     * @return \DateTimeImmutable
+     */
     public function getDate(): \DateTimeImmutable
     {
         return $this->date;
     }
 
+    /**
+     * @return string
+     */
     public function getLocation(): string
     {
         return $this->location;
     }
 
+    /**
+     * @return string
+     */
     public function getFileName(): string
     {
         return $this->fileName;
