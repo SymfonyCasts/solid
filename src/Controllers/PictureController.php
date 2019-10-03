373b810107a24a735c7605711c16c976ed35f8a7
Mauro Chojrin <mauro.chojrin@leewayweb.com>
2019-10-03 11:18:52 -0300

    Moving usage of WebSiteRenderer inside appropriate methods

--- src/Controllers/PictureController.php
+++ src/Controllers/PictureController.php
@@ -2,7 +2,6 @@
 
 namespace sasquatch\Controllers;
 
-use sasquatch\Models\Picture;
 use sasquatch\Repositories\PictureRepository;
 use sasquatch\Services\PictureRenderer;
 use sasquatch\Services\UploadManager;
@@ -10,13 +9,6 @@ use sasquatch\Services\WebSiteRenderer;
 
 class PictureController
 {
-    private $webSiteRenderer;
-
-    public function __construct(WebSiteRenderer $renderer )
-    {
-        $this->webSiteRenderer = $renderer;
-    }
-
     public function index(): string
     {
         $contents = '
@@ -32,20 +24,22 @@ class PictureController
 
         $contents .= '</div>';
 
-        return $this->webSiteRenderer->renderPage('All Reported Sightings', $contents);
+        return (new WebSiteRenderer())->renderPage('All Reported Sightings', $contents);
     }
 
     public function upload(): string
     {
+        $webSiteRenderer = new WebSiteRenderer();
         if ('post' !== strtolower($_SERVER['REQUEST_METHOD'])) {
-            return $this->webSiteRenderer->renderUploadForm();
+
+            return $webSiteRenderer->renderUploadForm();
         }
 
         // Process form
         if (!array_key_exists('newPicture', $_FILES) || !array_key_exists('author', $_POST)) {
             http_send_status(400);
 
-                return $this->webSiteRenderer->renderPage( 'New sighting',"<p>Sorry... I didn't understand :(...</p><p>Wanna <a href='upload'>try again?</a></p>");
+                return $webSiteRenderer->renderPage( 'New sighting',"<p>Sorry... I didn't understand :(...</p><p>Wanna <a href='upload'>try again?</a></p>");
             } else {
                 $uploadManager = new UploadManager();
                 $pictureRepository = new PictureRepository();
@@ -58,14 +52,14 @@ class PictureController
                     );
                     $pictureRepository->save( $newPicture );
 
-                return $this->webSiteRenderer->renderPage('New sighting', '
+                return $webSiteRenderer->renderPage('New sighting', '
             <h2>Sasquatch Spotted! </h2>
             <p>Click <u><a class="text-white" href="/">here</a></u> to see your image and the rest of the sightings!</p>'
                 );
             } catch (\Exception $e) {
                 error_log(__FILE__.': '.$e->getMessage());
 
-                return $this->webSiteRenderer->renderPage('New sighting!', '
+                return $webSiteRenderer->renderPage('New sighting!', '
         <h2 class="mb-4 text-center">Sneaky like a Sasquatch, that upload didn\'t work!</h2>
         <p class="text-center"><a class="btn btn-light btn-lg" href="upload">Try Again</a></p>
         '
