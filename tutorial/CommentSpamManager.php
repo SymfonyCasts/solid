984e9800f4f1ab534bff77ab72cf597dcdba5a2b
Diego Aguiar <barret91@hotmail.com>
2021-02-25 15:13:50 -0700

    Add CommentSpamManager

---
+++ tutorial/CommentSpamManager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Manager;
+
+use App\Entity\Comment;
+
+class CommentSpamManager
+{
+    public function validate(Comment $comment)
+    {
+        $content = $comment->getContent();
+        $badWordsOnComment = [];
+
+        $regex = implode('|', $this->spamWords());
+
+        preg_match("/$regex/", $content, $badWordsOnComment);
+
+        if (count($badWordsOnComment) >= 2) {
+            // We could throw a custom exception if needed
+            throw new \RuntimeException('Message detected as spam');
+        }
+    }
+
+    private function spamWords(): array
+    {
+        return [
+            'follow me',
+            'twitter',
+            'facebook',
+            'earn money',
+            'SymfonyCasts',
+        ];
+    }
+}
