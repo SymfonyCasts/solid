c0e937fad9efd229e066f71b9a751b146e0ea50d
Diego Aguiar <barret91@hotmail.com>
2021-03-05 11:48:00 -0700

    Fix evaluateCoordinates, wrong variable on if

--- src/Service/SightingScorer.php
+++ src/Service/SightingScorer.php
@@ -26,7 +26,7 @@ class SightingScorer
         // California edge to edge coordinates
         if (true
             && $lat >= 32.5121 && $lat <= 42.0126
-            && $lng >= -114.1315 && $lat <= -124.6509
+            && $lng >= -114.1315 && $lng <= -124.6509
         ) {
             $score += 30;
         }
